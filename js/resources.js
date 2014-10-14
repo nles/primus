7c8ebdfb3d22504f3681c832c260196952b77f34
Nikke <nileskel@gmail.com>
2014-10-14 16:57:31 +0300

    initial

---
+++ js/resources.js
@@ -0,0 +1,27 @@
+game.resources = [
+
+  /* Graphics. */
+  {name: "sewer_tileset", type:"image", src: "data/img/map/sewer_tileset.png"},
+  // metatiles
+  {name: "metatiles24x24",  type:"image", src: "data/img/map/metatiles24x24.png"},
+  // the main player spritesheet
+  {name: "mover_right", type:"image", src: "data/img/sprite/mover_right.png"},
+
+  /* Atlases
+   * @example
+   * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+   */
+
+  /* Maps. */
+  {name: "area01", type: "tmx", src: "data/map/area01.tmx"},
+
+  /* Background music.
+   * @example
+   * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+   */
+
+  /* Sound effects.
+   * @example
+   * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+   */
+];
